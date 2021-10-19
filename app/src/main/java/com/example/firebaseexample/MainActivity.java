86fabb5a6f22ee3cbd27125c23174950ff403add
Frank David Addae <frankdavid.addae@gmail.com>
2021-10-19 11:57:20 +0000

    sending data to firebase database

--- app/src/main/java/com/example/firebaseexample/MainActivity.java
+++ app/src/main/java/com/example/firebaseexample/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.firebaseexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etData;
+    Button btnSendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etData = findViewById(R.id.etData);
+        btnSendData = findViewById(R.id.btnSendData);
+
+        btnSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = etData.getText().toString();
+
+                // Get firebase database instance and save the data
+                FirebaseDatabase.getInstance().getReference("data").setValue(data)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Success!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
